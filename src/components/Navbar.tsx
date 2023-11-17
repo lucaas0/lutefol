89d7510fd352872bdeabcd11a19e4b95a5b72553
Lucas Garcia <lukas_garcia13@hotmail.com>
2023-11-17 22:29:23 +0000

    Fixed warning and navbar error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const Navbar = () => {
     return (
-        <navbar className='flex justify-between w-full py-10'>
+        <div className='flex justify-between w-full py-10'>
             <section className='flex gap-4 items-center'>
                 <Image
                     src='/logo.svg'
@@ -17,7 +17,7 @@ const Navbar = () => {
                 <Image src='/search-ic.svg' alt='' width={24} height={24} />
                 <Image src='/menu-ic.svg' alt='' width={24} height={24} />
             </section>
-        </navbar>
+        </div>
     );
 };
 
