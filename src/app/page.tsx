c8c8f9be734d08cd7f9801993d51c78954da1ad6
Lucas Garcia <lukas_garcia13@hotmail.com>
2023-11-17 22:34:49 +0000

    Added some responsiviness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import PlayersRow from '@/components/PlayersRow';
 
 export default function Home() {
     return (
-        <main className='flex min-h-screen flex-col items-center px-20'>
+        <main className='flex min-h-screen flex-col items-center px-5 md:px-10 lg:px-20'>
             <Navbar />
             <PlayersRow />
             <hr className='fade-hr' />
