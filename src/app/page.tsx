2c2fad72cb294be4c55373b407fd8ac24dbb624b
Lucas Garcia <lucas.garcia@voidsoftware.com>
2023-12-17 18:49:50 +0000

    Added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Navbar from '@/components/Navbar';
-import Players from '@/components/Players';
 
 export default function Home() {
     return (
-        <main className='flex min-h-screen flex-col items-center px-5 md:px-10 lg:px-20'>
+        <main className='flex min-h-screen flex-col items-center'>
             <Navbar />
-            <Players />
         </main>
     );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,42 @@
 import Image from 'next/image';
+import '../assets/styles/navbar.css';
+import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between w-full py-10'>
-            <section className='flex gap-4 items-center'>
-                <Image
-                    src='/logo.svg'
-                    alt=''
-                    width={24}
-                    height={24}
-                    className='bg-white'
-                />
-                lutefol
-            </section>
-            <section className='flex gap-4 items-center'>
-                <Image src='/search-ic.svg' alt='' width={24} height={24} />
-                <Image src='/menu-ic.svg' alt='' width={24} height={24} />
-            </section>
-        </div>
+            <div className='flex justify-between w-full px-20 navbar'>
+                <section className='flex gap-5 items-center'>
+                    <Image
+                        src='/pirates-logo.svg'
+                        alt=''
+                        width={48}
+                        height={48}
+                        className="cursor-pointer"
+                    />
+                    <Image
+                        src='/void-logo.svg'
+                        alt=''
+                        width={60}
+                        height={60}
+                    />
+
+                    <hr className="divider" />
+
+                    <ul>
+                    <li className='link-effect link-effect__selected'>News</li>
+                        <li className='link-effect'>Matches</li>
+                        <li className='link-effect'>Team</li>
+                        <li className='link-effect'>Stats</li>
+                        <li className='link-effect'>Media</li>
+                    </ul>
+                </section>
+                <section className='flex gap-8 items-center'>
+                    <Image src='/search-ic.svg' alt='' width={24} height={24} />
+                    <Image src='/ic-user.svg' alt='' width={24} height={24} />
+                    <Image src='/ic-globe.svg' alt='' width={24} height={24} />
+                    <Link href="/" className='navbar__link'> SIGN IN </Link>
+                </section>
+            </div>
     );
 };
 
