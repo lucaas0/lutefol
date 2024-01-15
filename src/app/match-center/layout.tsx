977110f46f3c018ceb854fb10a3a5d5b2e5e1023
Lucas Garcia <lucas.garcia@voidsoftware.com>
2024-01-15 16:22:20 +0000

    Team name uppercased

--- src/app/match-center/layout.tsx
+++ src/app/match-center/layout.tsx
@@ -45,7 +45,7 @@ export default function MatchesLayout({
                             </div>
                             <div className="center-absolute bottom-8 md:bottom-24 flex items-center gap-6">
                                 <div className="flex flex-col-reverse md:flex-row items-center gap-4 md:gap-8">
-                                    <h2 className="font-bold text-xl md:text-2xl text-center md:text-left">{match.team1.name}</h2>
+                                    <h2 className="font-bold uppercase text-xl md:text-2xl text-center md:text-left">{match.team1.name}</h2>
                                     <Image src={match.team1.logo} alt="" width={52} height={52} />
                                 </div>
                                 {
@@ -64,7 +64,7 @@ export default function MatchesLayout({
                                 }
                                 <div className="flex flex-col md:flex-row items-center gap-4 md:gap-8">
                                     <Image src={match.team2.logo} alt="" width={52} height={52} />
-                                    <h2 className="font-bold text-xl md:text-2xl text-center md:text-left">{match.team2.name}</h2>
+                                    <h2 className="font-bold uppercase text-xl md:text-2xl text-center md:text-left">{match.team2.name}</h2>
                                 </div>
                             </div>
                         </React.Fragment>
