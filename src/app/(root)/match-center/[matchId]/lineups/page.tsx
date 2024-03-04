880101cf357f772159f3782cff786d5bd1d6d0d5
Lucas Januario <lukasgarciaj@gmail.com>
2024-03-04 15:16:17 +0000

    initial btn

--- src/app/(root)/match-center/[matchId]/lineups/page.tsx
+++ src/app/(root)/match-center/[matchId]/lineups/page.tsx
@@ -1,22 +1,34 @@
 'use client';
-import { getMatchById } from '@/utils';
+import { PlayersArr, getMatchById } from '@/utils';
 import { useParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../../../../assets/styles/matchCenter.css';
 import Image from 'next/image';
 import { Match } from '@/misc';
+import { Session } from 'next-auth';
+import { getSession } from 'next-auth/react';
+import { Modal } from '@/components/Modal';
+import Players from '@/components/Players';
 
 const MatchLineup = () => {
     const params: { matchId: string } = useParams();
 
     const [match, setMatch] = useState<Match | undefined>(undefined);
+    const [session, setSession] = useState<Session | null>(null);
+    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
         const getMatch = () => {
             const m = getMatchById(params.matchId);
             setMatch(m);
         }
+
+        const defineSession = async () => {
+            const s = await getSession();
+            setSession(s);
+        }
         getMatch();
+        defineSession();
     }, [params.matchId]);
 
     const renderLineups = () => {
@@ -50,10 +62,40 @@ const MatchLineup = () => {
         })
     }
 
+    const renderAddLineupsBtn = () => {
+        return (
+            <button className='px-4 py-2 md:px-8 md:py-4 bg-white rounded-full text-black text-base font-roboto' onClick={() => setShowModal(true)}>
+                Add lineups
+            </button>
+        )
+    }
+
     return (
-        <div className='flex gap-3 m-6'>
-            {renderLineups()}
-        </div>
+        <React.Fragment>
+            {
+                showModal && (
+                    <Modal.Root containerClass='flex flex-col'>
+                        <Modal.Header title='Create lineups' handleClose={() => setShowModal(false)} />
+                        <Modal.Content>
+                            <div className='flex flex-col overflow-scroll gap-4 my-8'>
+                                {PlayersArr.map((p) => {
+                                    return (
+                                        <div className='flex gap-2 font-roboto' key={`player${p.nickName}`}>
+                                            <span className='w-8 color-brand-400'>{ p.clubNumber }</span>
+                                            <span>{ p.nickName }</span>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </Modal.Content>
+                    </Modal.Root>
+                )
+            }
+            <div className='flex gap-3 m-6'>
+                {renderLineups()}
+                { session && session.user && renderAddLineupsBtn() }
+            </div>
+        </React.Fragment>
     )
 };
 
